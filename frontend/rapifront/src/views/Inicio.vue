65545dd27dec84775dc1185ebd8db0d11f040276
DiegoKraenau <diegokraenau@gmail.com>
2019-11-11 23:21:31 -0500

    Mejorando frontend

--- frontend/rapifront/src/views/Inicio.vue
+++ frontend/rapifront/src/views/Inicio.vue
@@ -16,32 +16,40 @@
   </ul>
 </nav>
 
-<div class="p-3">
-            <img class="img-fluid rounded-circle" src="../assets/Logo.jpg" id="ImagenLogo" >
-</div>
 
-<form id="formulario">
-		<div class="form-group" >
-			<label for="exampleInputEmail1">Email address</label> 
-			<input type="email" class="form-control col-lg-3 col-md-3 col-xs-12" id="exampleInputEmail1" aria-describedby="emailHelp"
-				placeholder="Enter email" v-model="logeo.userName"> <small id="emailHelp"
-				class="form-text text-muted">We'll never share your email
-				with anyone else.</small>
-		</div>
-		<div class="form-group">
-			<label for="exampleInputPassword1">Password</label> <input
-				type="password" class="form-control col-lg-3 col-md-3 col-xs-12"
-				id="exampleInputPassword1" placeholder="Password" v-model="logeo.userPassword">	
-		</div>
 
-		<button type="submit" class="btn btn-primary" v-on:click.prevent="post" >Ingresar</button>
-		<br>
-		<br>
-		<a class="btn btn-secondary" href="http://localhost:8080/agregarUsuario">Registrarse</a>
 
-		
-</form>
+<div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Sign In</h5>
+            <form class="form-signin">
+              <div class="form-label-group">
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus v-model="logeo.userName">
+                <label for="inputEmail">Email address</label>
+              </div>
 
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="logeo.userPassword">
+                <label for="inputPassword">Password</label>
+              </div>
+
+              <div class="custom-control custom-checkbox mb-3">
+                <input type="checkbox" class="custom-control-input" id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Remember password</label>
+              </div>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit"  v-on:click.prevent="post">Sign in</button>
+              <hr class="my-4">
+              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit" v-on:click.prevent="registrar"><i class="fab fa-google mr-2"></i> Sign up</button>
+            
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 </body>
@@ -81,6 +89,9 @@ data(){
 			
              if ((element.userName == name)&& (element.userPassword == contraseña)) {window.location.href="Principal/"+element.usuarioId}	   
          });
+		},
+		registrar:function(){
+		window.location.href="http://localhost:8080/agregarUsuario"
 		}
     }
 }
@@ -99,6 +110,142 @@ data(){
     position: relative !important;
 	left: 30% !important;
     
+}
+
+:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: .75rem;
+}
+
+body {
+  background: #007bff;
+  background: linear-gradient(to right, #f1e784, #faf8ee);
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 2rem;
+}
+
+.form-label-group>input,
+.form-label-group>label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group>label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown)~label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+
+.btn-google {
+  color: white;
+  background-color: #ea4335;
+}
+
+.btn-facebook {
+  color: white;
+  background-color: #3b5998;
+}
+
+/* Fallback for Edge
+-------------------------------------------------- */
+
+@supports (-ms-ime-align: auto) {
+  .form-label-group>label {
+    display: none;
+  }
+  .form-label-group input::-ms-input-placeholder {
+    color: #777;
+  }
+}
+
+/* Fallback for IE
+-------------------------------------------------- */
+
+@media all and (-ms-high-contrast: none),
+(-ms-high-contrast: active) {
+  .form-label-group>label {
+    display: none;
+  }
+  .form-label-group input:-ms-input-placeholder {
+    color: #777;
+  }
 }
 
 </style>
